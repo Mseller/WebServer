36b884c6161b9b9c88db3fa5029983eba0bf820f
Seller, Martin <martinseller@live.com>
2017-11-09 11:55:03 +0900

    First commit

---
+++ src/WebServer.java
@@ -0,0 +1,8 @@
+
+// gist.github.com/kunst1080/7ac5595e8a845aacbe4d (SimpleWebServer Example)
+// https://stackoverflow.com/questions/3828352/what-is-a-mime-type (MIMETYPE)
+// https://www.codeproject.com/Tips/1040097/Create-a-Simple-Web-Server-in-Java-HTTP-Server (HTTPServer example)
+// 
+
+public class WebServer {
+}
