a3d65f0f102148844e938255c08879342df83637
Seller, Martin <martinseller@live.com>
2017-11-12 00:56:05 +0900

    Removed unnecessary comments

--- src/WebServer/WebServer.java
+++ src/WebServer/WebServer.java
@@ -1,9 +1,4 @@
 package WebServer;
-// gist.github.com/kunst1080/7ac5595e8a845aacbe4d (SimpleWebServer Example)
-// https://stackoverflow.com/questions/3828352/what-is-a-mime-type (MIMETYPE)
-// https://www.codeproject.com/Tips/1040097/Create-a-Simple-Web-Server-in-Java-HTTP-Server (HTTPServer example)
-// https://examples.javacodegeeks.com/core-java/sun/net-sun/httpserver-net-sun/httpserver-net-sun-httpserver-net-sun/com-sun-net-httpserver-httpserver-example/ (Good httpserver example)
-
 
 import java.io.IOException;
 
